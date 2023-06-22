d15787a0c5c78de291185204d1f8031d1b0a60d6
Christian <filetelopezchristian@gmail.com>
2023-06-22 18:13:21 -0500

    Se solucionaron problemas

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ export const Card = ({ red, icon, followers, today, name, color }) => {
   return (
     <article className='top-bar bg-LightGrayishBlue rounded-md shadow-md overflow-hidden dark:bg-DarkDesaturatedBlue cursor-pointer hover:bg-LightGrayishBlueHover dark:hover:bg-DarkDesaturatedBlueHover'>
       <a href='/' className='mt-[2.1rem] block text-center text-[.75rem] font-inter font-[700] text-DarkGrayishBlue dark:text-DesaturatedBlue'>
-        <img src={red} alt='' aria-disabled='true' className='inline-block mr-3' />
+        <img src={red} alt='' role='none' className='inline-block mr-3' />
         {name}
       </a>
       <h1 className='text-center mt-[1.4rem] font-inter font-[700] text-[3.5rem] leading-[1] text-VeryDarkBlueText dark:text-WhiteText'>
@@ -15,7 +15,7 @@ export const Card = ({ red, icon, followers, today, name, color }) => {
         FOLLOWERS
       </p>
       <p className={color === 'red' ? 'mt-[1.2rem] mb-[1.3rem] text-center font-inter font-[700] text-[.83rem] text-brightRed' : 'mt-[1.2rem] mb-[1.3rem] text-center font-inter font-[700] text-[.83rem] text-limeGreen'}>
-        <img src={icon} alt='' className='inline-block ml-[.3rem]' aria-disabled='true' />
+        <img src={icon} alt='' className='inline-block ml-[.3rem]' role='none' />
         {today}
       </p>
     </article>
