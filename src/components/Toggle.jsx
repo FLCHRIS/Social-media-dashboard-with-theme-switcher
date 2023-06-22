b7dc050b45c619db43f54d339c3c9c1c8890469e
Christian <filetelopezchristian@gmail.com>
2023-06-22 18:21:59 -0500

    Se solucionaron problemas

--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -3,9 +3,9 @@ import PropTypes from 'prop-types'
 
 export const Toggle = ({ handleTheme, dark }) => {
   return (
-    <button aria-label='Button to change theme' onClick={handleTheme} className={dark === 'dark' ? 'bg-gradient-to-r from-toggleLight to-toggleLight dark:bg-gradient-to-r dark:from-toggleDarkFrom dark:to-toggleDarkTo p-[.25rem] w-[3rem] h-[1.5rem] rounded-xl flex items-center justify-start' : 'bg-gradient-to-r from-toggleLight to-toggleLight dark:bg-gradient-to-r dark:from-toggleDarkFrom dark:to-toggleDarkTo p-[.25rem] w-[3rem] h-[1.5rem] rounded-xl flex items-center justify-end'}>
-      <div className='bg-white dark:bg-VeryDarkBlueTop w-[1.1rem] h-[1.1rem] rounded-full' />
-    </button>
+    <div aria-label='Button to change theme' className={dark === 'dark' ? 'bg-gradient-to-r from-toggleLight to-toggleLight dark:bg-gradient-to-r dark:from-toggleDarkFrom dark:to-toggleDarkTo p-[.25rem] w-[3rem] h-[1.5rem] rounded-xl flex items-center justify-start' : 'bg-gradient-to-r from-toggleLight to-toggleLight dark:bg-gradient-to-r dark:from-toggleDarkFrom dark:to-toggleDarkTo p-[.25rem] w-[3rem] h-[1.5rem] rounded-xl flex items-center justify-end'}>
+      <button onClick={handleTheme} className='bg-white dark:bg-VeryDarkBlueTop w-[1.1rem] h-[1.1rem] rounded-full' />
+    </div>
   )
 }
 Toggle.propTypes = {
