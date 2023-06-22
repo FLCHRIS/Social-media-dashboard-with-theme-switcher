35caf5f601e1b2ddbf4c456a0101fd55ff28f7b9
Christian <filetelopezchristian@gmail.com>
2023-06-22 18:29:18 -0500

    Se solucionaron problemas

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -6,12 +6,12 @@ export const View = ({ title, red, number, today, color, icon }) => {
     <article className='bg-LightGrayishBlue cursor-pointer hover:bg-LightGrayishBlueHover px-[1.5rem] pt-[1.53rem] pb-[.94rem] rounded-md shadow-md dark:bg-DarkDesaturatedBlue dark:hover:bg-DarkDesaturatedBlueHover'>
       <h2 className='flex justify-between items-center font-inter font-[700] text-[.89rem] text-DarkGrayishBlue dark:text-DesaturatedBlue'>
         {title}
-        <img src={red} alt='' role='none' className='mr-[.3rem]' />
+        <img src={red} alt='' role='none presentation' className='mr-[.3rem]' />
       </h2>
       <p className='font-inter font-[700] text-VeryDarkBlueText mt-[1rem] text-[2rem] flex justify-between items-center dark:text-WhiteText'>
         {number}
         <span className={color === 'green' ? 'text-[.8rem] mr-[.3rem] mt-[.9rem] text-limeGreen' : 'text-[.8rem] mr-[.3rem] mt-[.9rem] text-brightRed'}>
-          <img src={icon} alt='' role='none' className='inline-block' />
+          <img src={icon} alt='' role='none presentation' className='inline-block' />
           {today}
         </span>
       </p>
