ce2a83f7662b1e970f1998c3bb94de987dec1e41
Christian <filetelopezchristian@gmail.com>
2023-06-22 18:21:54 -0500

    Se solucionaron problemas

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -4,10 +4,10 @@ import PropTypes from 'prop-types'
 export const View = ({ title, red, number, today, color, icon }) => {
   return (
     <article className='bg-LightGrayishBlue cursor-pointer hover:bg-LightGrayishBlueHover px-[1.5rem] pt-[1.53rem] pb-[.94rem] rounded-md shadow-md dark:bg-DarkDesaturatedBlue dark:hover:bg-DarkDesaturatedBlueHover'>
-      <h1 className='flex justify-between items-center font-inter font-[700] text-[.89rem] text-DarkGrayishBlue dark:text-DesaturatedBlue'>
+      <h2 className='flex justify-between items-center font-inter font-[700] text-[.89rem] text-DarkGrayishBlue dark:text-DesaturatedBlue'>
         {title}
         <img src={red} alt='' role='none' className='mr-[.3rem]' />
-      </h1>
+      </h2>
       <p className='font-inter font-[700] text-VeryDarkBlueText mt-[1rem] text-[2rem] flex justify-between items-center dark:text-WhiteText'>
         {number}
         <span className={color === 'green' ? 'text-[.8rem] mr-[.3rem] mt-[.9rem] text-limeGreen' : 'text-[.8rem] mr-[.3rem] mt-[.9rem] text-brightRed'}>
